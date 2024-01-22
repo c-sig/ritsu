b001d88ffc958fd6f249d57fd0618ecde252f4d6
sisig <imsisig@pm.me>
2024-01-22 18:28:23 +0800

    remove pycache and other stuff

--- dbhandler.py
+++ dbhandler.py
@@ -1,4 +1,3 @@
-import peewee
 from peewee import *
 
 db = SqliteDatabase('ritsu.sqlite')
@@ -9,80 +8,56 @@ class BaseModel(Model):
         
 class User(BaseModel):
     id = IntegerField(primary_key=True)
-    join_date = DateTimeField()
+    joined = DateTimeField()
     origin = CharField()
     status = CharField()
     activity = CharField()
     
-    def create(id, join_date, origin, status, activity):
-        User.create(id=id, join_date=join_date, origin=origin, status=status, activity=activity)
-        
     def read(id):
-        return User.select().where(User.id == id)
+        return User.get(User.id == id)
     
-    def update(id, join_date, origin, status, activity):
-        User.update(id=id, join_date=join_date, origin=origin, status=status, activity=activity).where(User.id == id).execute()
+    def update(id, joined, origin, status, activity):
+        User.update(joined=joined, origin=origin, status=status, activity=activity).where(User.id == id).execute()
         
     def delete(id):
         User.delete().where(User.id == id).execute()
         
     def list():
-        #lists all users
-        
-        for user in User.select():
-            users = []
-            #add all data of a user to a list in a list
-            users.append([user.id, user.join_date, user.origin, user.status, user.activity])
-            
-        return users
+        return User.select()
     
 class Qualification(BaseModel):
     id = CharField(primary_key=True)
-    category = CharField()
+    
+    def read(id):
+        return Qualification.get(Qualification.id == id)
+    
+    def update(id):
+        Qualification.update(id=id).where(Qualification.id == id).execute()
+        
+    def delete(id):
+        Qualification.delete().where(Qualification.id == id).execute()
+        
+    def list():
+        return Qualification.select()
     
 class UserQualification(BaseModel):
     id = IntegerField(primary_key=True)
-    user_id = ForeignKeyField(User, backref='qualifications')
-    qualification_id = ForeignKeyField(Qualification, backref='users')
+    user_id = ForeignKeyField(User, related_name='qualifications')
+    qualification_id = ForeignKeyField(Qualification, related_name='users')
     
-class Group(BaseModel):
-    id = IntegerField(primary_key=True)
-    lead = ForeignKeyField(User, backref='groups')
-    creation_date = DateTimeField()
+    def read(id):
+        return UserQualification.get(UserQualification.id == id)
     
-class Series(BaseModel):
-    id = IntegerField(primary_key=True)
-    order_id = DeferredForeignKey('Order', backref='series')
-    group_id = ForeignKeyField(Group, backref='series')
-    manager = ForeignKeyField(User, backref='series')
-
-class Chapter(BaseModel):
-    id = IntegerField(primary_key=True)
-    series_id = ForeignKeyField(Series, backref='chapters')
-    status = CharField()
-    started = DateTimeField()
-    finished = DateField()
+    def update(id, user_id, qualification_id):
+        UserQualification.update(user_id=user_id, qualification_id=qualification_id).where(UserQualification.id == id).execute()
+        
+    def delete(id):
+        UserQualification.delete().where(UserQualification.id == id).execute()
+        
+    def list():
+        return UserQualification.select()
     
-class Job(BaseModel):
-    id = IntegerField(primary_key=True)
-    chapter_id = ForeignKeyField(Chapter, backref='jobs')
-    assigned_user = ForeignKeyField(User, backref='jobs')
-    started = DateTimeField()
-    finished = DateTimeField()
-    locked = BooleanField()
-    
-class JobQualification(BaseModel):
-    id = IntegerField(primary_key=True)
-    qualification_id = ForeignKeyField(Qualification, backref='qualified')
-
-class Order(BaseModel):
-    id = IntegerField(primary_key=True)
-    order = IntegerField()
-    job_id = ForeignKeyField(Job, backref='orders')
-
-
 db.connect()
-if not db.table_exists('user'):
-    db.create_tables([User, Qualification, UserQualification, Group, Series, Chapter, Job, JobQualification, Order])
-    
-print(User.list())
+
+db.create_tables([User, Qualification, UserQualification])
+
--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import os
 import discord
+from dbhandler import db, User, Qualification, UserQualification
 
+bot = discord.Bot()
 
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+    
+#ping command
+@bot.slash_command(name="ping", description="Measure ping")
+async def ping(ctx):
+    await ctx.respond(f"Pong! ({bot.latency*1000}ms)")
+    
 
 TOKEN = os.environ.get('RITSU_TOKEN')
+bot.run(TOKEN)
 
